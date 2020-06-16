03b41d55573fea001c90ae7eda1b84c27b3ce8b7
Karuka3 <karuka.piplup.0327@gmail.com>
2020-06-17 04:00:14 +0900

    Change folder and Fix Joint Topic Model

--- src/TopicModel.py
+++ src/TopicModel.py
@@ -29,7 +29,8 @@ class PLSA:
                 for n in range(len(self.W[d])):
                     for k in range(self.K):
                         self.q[d][k][n] = self.e_step(d, n, k)
-                        theta_new, phi_new = self.m_step(d, n, k, theta_new, phi_new)
+                        theta_new, phi_new = self.m_step(
+                            d, n, k, theta_new, phi_new)
             self.theta /= self.theta.sum(axis=1)[:, np.newaxis]
             self.phi /= self.phi.sum(axis=1)[:, np.newaxis]
             self.phi[self.phi < 1e-100] = 1e-100
@@ -101,11 +102,10 @@ class PLSA:
 
 
 class LDA:
-    def __init__(self, K=9, alpha=0.1, beta=0.1, verbose=0, random_state=17):
+    def __init__(self, K=9, alpha=0.1, beta=0.1, random_state=717):
         self.K = K          # <- topic count
         self.alpha = alpha  # <- parameter of topics prior
         self.beta = beta    # <- parameter of words proir
-        self.verbose = verbose
         self.random_state = random_state
 
     def fit(self, docs, word2num, estimator="Gibbs", max_iter=1000):
@@ -113,11 +113,11 @@ class LDA:
         self.W = docs
         self.V = len(word2num)  # <- word count
         self.D = len(docs)      # <- documents count
-        self.topics = self.init_topics()
+        self.Z = self.init_topics()
         self.ndk, self.nkv, self.nd, self.nk = self.init_params()
 
         for __iter in tqdm(range(max_iter)):
-            for i, d in enumerate(self.topics):
+            for i, d in enumerate(self.Z):
                 for j, k in enumerate(d):
                     v = self.W[i][j]
                     self.ndk[i, k] -= 1
@@ -125,19 +125,21 @@ class LDA:
                     self.nk[k] -= 1
                     new_z = self.sampling(i, v)
                     # update topics
-                    self.topics[i][j] = new_z
+                    self.Z[i][j] = new_z
                     self.ndk[i, new_z] += 1
                     self.nkv[new_z, v] += 1
                     self.nk[new_z] += 1
-        save = {"topics": self.topics, "nkv": self.nkv,
+        save = {"topics": self.Z, "nkv": self.nkv,
                 "ndk": self.ndk, "nk": self.nk, "nd": self.nd}
         return save
 
     def sampling(self, i, v):
         probs = np.zeros(self.K)
         for k in range(self.K):
-            theta = (self.ndk[i, k] + self.alpha) / (self.nd[i] + self.alpha * self.D)
-            phi = (self.nkv[k, v] + self.beta) / (self.nk[k] + self.beta * self.K)
+            theta = (self.ndk[i, k] + self.alpha) / \
+                (self.nd[i] + self.alpha * self.D)
+            phi = (self.nkv[k, v] + self.beta) / \
+                (self.nk[k] + self.beta * self.K)
             prob = theta * phi
             probs[k] = prob
         probs /= probs.sum()
@@ -151,7 +153,7 @@ class LDA:
     def init_params(self):
         ndk = np.zeros((self.D, self.K))  # <- topic distribution of sentences
         nkv = np.zeros((self.K, self.V))  # <- word disrtibution of each topics
-        for i, d in enumerate(self.topics):
+        for i, d in enumerate(self.Z):
             for j, z in enumerate(d):
                 ndk[i, z] += 1
                 nkv[z, self.W[i][j]] += 1
@@ -161,74 +163,70 @@ class LDA:
 
 
 class JointTopicModel:
-    def __init__(self, K, alpha, beta, max_iter, verbose=0):
+    def __init__(self, K, alpha, beta, random_state=717):
         self.K = K
         self.alpha = alpha
         self.beta = beta
-        self.max_iter = max_iter
-        self.verbose = verbose
+        np.random.seed(random_state)
 
-    def fit(self, X, V):
-        self._X = X
-        self._T = len(X)  # number of vocab types
-        self._N = len(X[0])  # number of documents
-        self._V = V  # number of vocabularies for each t
-        self.Z = self._init_topics()
-        self.ndk, self.nkv = self._init_params()
-        nk = {}
-        for t in range(self._T):
-            nk[t] = self.nkv[t].sum(axis=1)
+    def fit(self, docs, word2num, category, max_iter=1000):
+        self.W = docs
+        self.T = len(docs)
+        self.D = len(docs[0])
+        self.V = [len(word2num), len(category)]
+        self.Z = self.init_topics()
+        self.ndk, self.nkv, self.nk, self.nd = self.init_params()
 
-        remained_iter = self.max_iter
-        while True:
-            if self.verbose:
-                print(remained_iter)
-            for t in np.random.choice(self._T, self._T, replace=False):
-                for d in np.random.choice(self._N, self._N, replace=False):
-                    for i in np.random.choice(len(self._X[t][d]), len(self._X[t][d]), replace=False):
+        for __iter in tqdm(range(max_iter)):
+            for t in range(self.T):
+                for d in np.random.choice(self.D, self.D, replace=False):
+                    for i in np.random.choice(len(self.W[t][d]), len(self.W[t][d]), replace=False):
                         k = self.Z[t][d][i]
-                        v = self._X[t][d][i]
+                        v = self.W[t][d][i]
 
                         self.ndk[t][d][k] -= 1
                         self.nkv[t][k][v] -= 1
-                        nk[t][k] -= 1
+                        self.nk[t][k] -= 1
 
-                        self.Z[t][d][i] = self._sample_z(t, d, v, nk[t])
-                        self.ndk[t][d][self.Z[t][d][i]] += 1
-                        self.nkv[t][self.Z[t][d][i]][v] += 1
-                        nk[t][self.Z[t][d][i]] += 1
-            remained_iter -= 1
-            if remained_iter <= 0:
-                break
-        return self
+                        new_z = self.sampling(t, d, v, self.nk[t])
+                        self.Z[t][d][i] = new_z
+                        self.ndk[t][d][new_z] += 1
+                        self.nkv[t][new_z][v] += 1
+                        self.nk[t][new_z] += 1
 
-    def _init_topics(self):
+        save = {"topics": self.Z, "nkv": self.nkv,
+                "ndk": self.ndk, "nk": self.nk, "nd": self.nd}
+        return save
+
+    def init_topics(self):
         Z = {}
-        for t in range(self._T):
-            Z[t] = []
-            for d in range(len(self._X[t])):
-                Z[t].append(np.random.randint(
-                    low=0, high=self.K, size=len(self._X[t][d])))
+        for t in range(self.T):
+            Z[t] = [np.random.randint(low=0, high=self.K, size=len(
+                self.W[t][d])) for d in range(len(self.W[t]))]
         return Z
 
-    def _init_params(self):
+    def init_params(self):
         ndk = {}
         nkv = {}
-        for t in range(self._T):
-            ndk[t] = np.zeros((self._N, self.K)) + self.alpha
-            nkv[t] = np.zeros((self.K, self._V[t])) + self.beta
-            for d in range(self._N):
-                for i in range(len(self._X[t][d])):
+        nk = {}
+        nd = {}
+        for t in range(self.T):
+            ndk[t] = np.zeros((self.D, self.K)) + self.alpha
+            nkv[t] = np.zeros((self.K, self.V[t])) + self.beta
+            for d in range(self.D):
+                for i in range(len(self.W[t][d])):
                     k = self.Z[t][d][i]
-                    v = self._X[t][d][i]
+                    v = self.W[t][d][i]
                     ndk[t][d, k] += 1
                     nkv[t][k, v] += 1
-        return ndk, nkv
+            nk[t] = nkv[t].sum(axis=1)
+            nd[t] = ndk[t].sum(axis=1)
+        return ndk, nkv, nk, nd
 
-    def _sample_z(self, t, d, v, nk):
+    def sampling(self, t, d, v, nk):
         nkv = self.nkv[t][:, v]  # k-dimensional vector
-        prob = (sum([self.ndk[t][d] for t in range(self._T)]) -
-                self.alpha*(self._T-1)) * (nkv/nk)
+        prob = (sum([self.ndk[t][d] for t in range(self.T)]) -
+                self.alpha*(self.T-1)) * (nkv/nk)
         prob = prob/prob.sum()
         z = np.random.multinomial(n=1, pvals=prob).argmax()
         return z
